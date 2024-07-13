5ff31c983bf531bcb899843a7fe9643a14657019
steven_savarin <srssdesing@gmail.com>
2024-07-13 15:11:03 +0200

    dev05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,13 +7,13 @@ class Program
     {
         List<Shape> shapes = new List<Shape>();
 
-        Square s1 = new Square("Red", 9);
+        Square s1 = new Square("Green", 9);
         shapes.Add(s1);
 
-        Rectangle r1 = new Rectangle("Orange", 12, 8);
+        Rectangle r1 = new Rectangle("Red", 12, 8);
         shapes.Add(r1);
 
-        Circle c1 = new Circle("Blue", 5);
+        Circle c1 = new Circle("Purple", 5);
         shapes.Add(c1);
 
         foreach (Shape s in shapes)
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.WriteLine("The Eternal Goal Setting Quest");
+        GoalManager goalManager = new();
+        goalManager.Start();
     }
 }
